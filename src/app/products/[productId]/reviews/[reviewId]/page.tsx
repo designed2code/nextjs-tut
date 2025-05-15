629073ed5a23a997ed1e1fd07fb5f1ed8f97d0d9
Sachin <sachin@Sachins-MacBook-Air.local>
2025-05-16 02:35:34 +0530

    Lec-10 Programatically invoking not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,13 @@
+import { notFound } from "next/navigation";
 export default async function ProductReview({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
   const { productId, reviewId } = await params;
+  if (parseInt(reviewId) > 500) {
+    notFound();
+  }
   return (
     <h1>
       Review {reviewId} for Product {productId}
