2fe80646e271b31edf7704679c9ebe5bbb17ca47
Sachin <sachin@Sachins-MacBook-Air.local>
2025-05-16 02:34:22 +0530

    Lec-10 Not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1>Page Not Found</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam
+        tempore a, perspiciatis facere labore vel nesciunt vitae deleniti,
+        accusamus inventore provident dolorem voluptatum aut maiores
+        praesentium. Exercitationem, distinctio eos?
+      </p>
+    </>
+  );
+}
